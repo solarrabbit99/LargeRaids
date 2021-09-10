90c6be7158ad3171993fb53259c67b5e65e084fa
Lee Zheng Han <e0544595@u.nus.edu>
2021-09-11 01:10:16 +0800

    Add artificial-only configuration option

--- largeraids-plugin/src/main/java/com/solarrabbit/largeraids/listener/TriggerListener.java
+++ largeraids-plugin/src/main/java/com/solarrabbit/largeraids/listener/TriggerListener.java
@@ -1,5 +1,6 @@
 package com.solarrabbit.largeraids.listener;
 
+import com.solarrabbit.largeraids.AbstractLargeRaid;
 import com.solarrabbit.largeraids.LargeRaids;
 import com.solarrabbit.largeraids.VersionUtil;
 import org.bukkit.Location;
@@ -10,11 +11,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 public abstract class TriggerListener implements Listener {
 
     protected void triggerRaid(Location loc, Player player) {
-        VersionUtil.createLargeRaid(loc, player).startRaid();
+        AbstractLargeRaid largeRaid = VersionUtil.createLargeRaid(loc, player);
+        largeRaid.startRaid();
+        if (!isAllowed(largeRaid.getCentre()))
+            largeRaid.stopRaid();
     }
 
     protected LargeRaids getPlugin() {
         return JavaPlugin.getPlugin(LargeRaids.class);
+    }
+
+    private boolean isAllowed(Location loc) {
+        return getPlugin().getConfig().getBoolean("artificial-only") ? isInDatabase(loc) : true;
+    }
+
+    private boolean isInDatabase(Location loc) {
+        return this.getPlugin().getDatabase().getCentres().values().stream()
+                .anyMatch(location -> location.distanceSquared(loc) < 3);
     }
 
 }
