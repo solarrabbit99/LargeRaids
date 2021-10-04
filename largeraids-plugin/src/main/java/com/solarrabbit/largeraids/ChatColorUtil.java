ab54421fd4c5cff3da096ef1420620a0197b0d1b
Lee Zheng Han <e0544595@u.nus.edu>
2021-10-04 20:56:33 +0800

    Delete unused class

--- largeraids-plugin/src/main/java/com/solarrabbit/largeraids/ChatColorUtil.java
+++
@@ -1,9 +0,0 @@
-package com.solarrabbit.largeraids;
-
-import org.bukkit.ChatColor;
-
-public class ChatColorUtil {
-    public static String translate(String str) {
-        return ChatColor.translateAlternateColorCodes('&', str);
-    }
-}
