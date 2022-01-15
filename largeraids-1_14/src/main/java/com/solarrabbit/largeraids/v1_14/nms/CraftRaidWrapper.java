aa844af7abb298ebcfd95f917e1ff551e29845dc
Lee Zheng Han <e0544595@u.nus.edu>
2022-01-15 15:59:23 +0800

    Use methodhandles for retrieving raid handles

--- largeraids-1_14/src/main/java/com/solarrabbit/largeraids/v1_14/nms/CraftRaidWrapper.java
+++ largeraids-1_14/src/main/java/com/solarrabbit/largeraids/v1_14/nms/CraftRaidWrapper.java
@@ -1,5 +1,7 @@
 package com.solarrabbit.largeraids.v1_14.nms;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 
 import com.solarrabbit.largeraids.nms.AbstractCraftRaidWrapper;
@@ -9,6 +11,7 @@ import org.bukkit.Raid;
 import org.bukkit.craftbukkit.v1_14_R1.CraftRaid;
 
 public class CraftRaidWrapper extends AbstractCraftRaidWrapper {
+    private static MethodHandle handle;
 
     public CraftRaidWrapper(AbstractRaidWrapper nmsRaid) {
         super(new CraftRaid(((RaidWrapper) nmsRaid).raid));
@@ -22,10 +25,14 @@ public class CraftRaidWrapper extends AbstractCraftRaidWrapper {
     public RaidWrapper getHandle() {
         net.minecraft.server.v1_14_R1.Raid nmsRaid;
         try {
-            Field field = CraftRaid.class.getDeclaredField("handle");
-            field.setAccessible(true);
-            nmsRaid = (net.minecraft.server.v1_14_R1.Raid) field.get(raid);
-        } catch (ReflectiveOperationException e) {
+            if (handle == null) {
+                Field field = CraftRaid.class.getDeclaredField("handle");
+                field.setAccessible(true);
+                handle = MethodHandles.lookup().unreflectGetter(field);
+            }
+            nmsRaid = (net.minecraft.server.v1_14_R1.Raid) handle.invokeExact((CraftRaid) raid);
+        } catch (Throwable e) {
+            e.printStackTrace();
             nmsRaid = null;
         }
         return new RaidWrapper(nmsRaid);
